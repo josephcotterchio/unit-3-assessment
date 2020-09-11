47372d3d6ebe2c069f573e7c1aecdb911d1999a8
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-09-11 17:41:23 -0400

    ugh

--- main_app/views.py
+++ main_app/views.py
@@ -2,22 +2,19 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from .forms import TaskForm
 from .models import Task
-from django.db.models import Sum
 
 # Create your views here.
 
 
 def home(request):
     task_list = Task.objects.all()
-    form = TaskForm(request.POST)
-    total = Task.objects.aggregate(Sum('time'))
-    if form.is_valid():
-        form.save()
+    task_form = TaskForm(request.POST)
+    if task_form.is_valid():
+        task_form.save()
         return redirect('home')
     else:
-        form = TaskForm()
-
-    return render(request, 'home.html', {'task_list': task_list, 'task_form': task_form, 'total': total})
+        task_form = TaskForm()
+    return render(request, 'home.html', {'task_list': task_list, 'task_form': task_form})
 
 
 def delete(request, task_id):
